58bbcd7aa1f797ab00e701dfd19103eb3b34aaa6
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-02-28 15:06:01 +0100

    Add Grid w/ Cells & Number Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,31 @@ console.log(grid)
 gridGenerator.addEventListener('click', function () {
     grid.style.display = 'flex'
 })
+
+//cicle for : cell Generation + index Number inside
+
+for (let i = 1; i < nCellMax + 1; i++) {
+    //cell
+    const cell = document.createElement('div')
+
+    cell.classList.add('cell');
+    grid.appendChild(cell);
+    /* cell.style.position = 'relative' */
+
+    //cellNumber
+    let cellNumber = document.createElement('div')
+    cellNumber.innerHTML = i
+
+    cellNumber.classList.add('cellnumber')
+    cell.appendChild(cellNumber)
+
+    /*  cellNumber.style.position = 'absolute'
+     cellNumber.style.top = '50%'
+     cellNumber.style.left = '50%'
+     cellNumber.style.transform = 'translate(-50%,-50%)' */
+    /* cellNumber.style.backgroundColor = 'black'
+    cellNumber.style.color = 'white'
+    cellNumber.style.borderRadius = '50px'
+    cellNumber.style.padding = '4px' */
+
+}
