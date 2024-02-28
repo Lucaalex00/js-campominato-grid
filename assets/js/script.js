2f3ec02d43c88df40472c1ab3517eaa2b49cc405
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-02-28 14:55:27 +0100

    add Header + Button commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//BUTTON : grid Generation
+let gridGenerator = document.getElementById('play');
+let grid = document.getElementById('grid')
+let nCellMax = 100;
+console.log(grid)
+
+gridGenerator.addEventListener('click', function () {
+    grid.style.display = 'flex'
+})
